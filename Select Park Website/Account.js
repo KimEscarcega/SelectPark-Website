76b7ca291e4e7847f155618043cd7dd7ca60ffb8
magalyarzate <magalyarzate32@gmail.com>
2024-03-04 10:27:03 -0600

    profile changes

--- Select Park Website/Account.js
+++ Select Park Website/Account.js
@@ -3,14 +3,39 @@ const img=document.querySelector('#photo');
 const file=document.querySelector('#file');
 const buttonupload=document.querySelector('#buttonupload');
 
-file.addEventListener('change', function(){
-    const chosedfile=this.files[0];
 
-    if(chosedfile){
-        const reader=new FileReader();
-        reader.addEventListener('load', function(){
-            img.setAttribute('src', reader.result);
-        });
-        reader.readAsDataURL(chosedfile);
+
+window.onload=function(){
+    const storedProfilePicURL=localStorage.getItem("profilePicURL");
+    if(storedProfilePicURL){
+        document.getElementById("photo").src=storedProfilePicURL;
+    }
+    else{
+        document.getElementById("photo").src="Images/avatar.jpg";
+    }
+};
+
+document.getElementById("file").addEventListener("change", function(event){
+    const file=event.target.files[0];
+    const reader=new FileReader();
+
+    reader.onload=function(){
+        const imageURL=reader.result;
+        document.getElementById("photo").src=imageURL;
+
+        localStorage.setItem("profilePicURL", imageURL);
+
     };
-})
+    reader.readAsDataURL(file);
+});
+
+document.getElementById("removeprofilepic").addEventListener("click", function(){
+    document.getElementById("photo").src="Images/avatar.jpg";
+    localStorage.removeItem("profilePicURL");
+});
+
+document.getElementById("saveprofilepic").addEventListener("click", function(){
+    const currentProfilePicURL=document.getElementById("photo").src;
+
+    alert("Profile picture saved" );
+});
